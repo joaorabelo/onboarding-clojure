f7fde9acad3cf6be060d1e0845ef30f0356075f2
Joao Filho <joao.filho@joao.filho>
2021-07-27 17:30:26 -0300

    add compras

--- src/onboarding_clojure/credit_card.clj
+++ src/onboarding_clojure/credit_card.clj
@@ -4,9 +4,34 @@
               :cpf "545.427.300-77"
               :email "paulo@gmail.com"
               :cartao {:numero 5137516643135932
-                       :validade "27/06/2023"
+                       :validade "27-06-2023"
                        :cvv 187
-                       :limite 2980}
+                       :limite 2980
+                       :compras [{:data "27-07-2021"
+                                 :valor 30.00
+                                 :estabelecimento "Restaurante Code"
+                                 :categoria "Alimentação"}
+                                 {:data "27-07-2021"
+                                  :valor 730.00
+                                  :estabelecimento "Restaurante Code"
+                                  :categoria "Alimentação"}
+                                 {:data "27-07-2021"
+                                  :valor 150.00
+                                  :estabelecimento "Edu Pay"
+                                  :categoria "Educação"}
+                                 {:data "27-07-2021"
+                                  :valor 1205.00
+                                  :estabelecimento "Edu Pay"
+                                  :categoria "Educação"}
+                                 {:data "27-07-2021"
+                                  :valor 250.00
+                                  :estabelecimento "Hospital A"
+                                  :categoria "Saúde"}
+                                 {:data "28-07-2021"
+                                  :valor 150.99
+                                  :estabelecimento "Hospital B"
+                                  :categoria "Saúde"}
+                                 ]}
               })
 
 (println cliente)
