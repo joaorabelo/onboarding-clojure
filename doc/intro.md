9def86e5071a116ea0b25cfd5af259446a3f42d7
Joao Filho <joao.filho@joao.filho>
2021-07-27 16:37:22 -0300

    base project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to onboarding-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/onboarding_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns onboarding-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [onboarding-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
