2388a673fa621c6780e8cd0274bbbb4d1e388531
Icaro Rios <icaro.rios@icaro.rios>
2021-07-27 17:08:43 -0300

    adcionando cartão

--- src/onboarding_clojure/credit_card.clj
+++ src/onboarding_clojure/credit_card.clj
@@ -2,4 +2,11 @@
 
 (def cliente {:nome "Paulo"
               :cpf "545.427.300-77"
-              :email "paulo@gmail.com"})
+              :email "paulo@gmail.com"
+              :cartao {:numero 5137516643135932
+                       :validade "27/06/2023"
+                       :cvv 187
+                       :limite 2980}
+              })
+
+(println cliente)
